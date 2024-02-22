add20a2d79a549cfce89c510f6084f1862cedd20
Soye <108269301+soyeya@users.noreply.github.com>
2024-02-22 15:22:24 +0900

    mock 서버 handler

---
+++ handlers.js
@@ -0,0 +1,30 @@
+import { rest } from "msw";
+import { database } from "./data";
+
+const RESPONSE_DELAY_MS = 1000;
+
+export const handlers = [
+
+  rest.post( process.env.PUBLIC_URL + "/api/order", (req, res, ctx) => {
+     const result = req.body;
+     console.log(typeof result);
+     const order = database.createOrder(JSON.parse(result));
+     return res(ctx.delay(RESPONSE_DELAY_MS), ctx.json(result));
+  }),
+
+  rest.get(process.env.PUBLIC_URL + "/api/order/my", (req, res, ctx) => {
+    const order = database.findOrder();
+    const index = Date.now() % 2;
+    order.status = ["음식 준비중", "배달중"][index];
+    order.position = [
+      [30, 30],
+      [60, 60],
+    ][index];
+    return res(ctx.delay(RESPONSE_DELAY_MS), ctx.json(order));
+  }),
+
+
+ 
+
+
+];
